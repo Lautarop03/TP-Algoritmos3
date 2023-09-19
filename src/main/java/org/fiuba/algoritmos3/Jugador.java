f1d651be1a9f9b140f3605dcc312060fa5a930ac
Lautarop03 <Lautaropedrozo2@gmail.com>
2023-09-19 20:55:08 -0300

    Agrego jugador

---
+++ src/main/java/org/fiuba/algoritmos3/Jugador.java
@@ -0,0 +1,24 @@
+package org.fiuba.algoritmos3;
+
+import java.util.List;
+
+public class Jugador {
+
+    private List<Pokemon> pokemones;
+
+    private List<Item> items;
+
+    public void usarHabilidad(){
+        return;
+    }
+
+    public void usarItem() {
+        return;
+    }
+    public void cambiarPokemon(){
+        return;
+    }
+    public void rendirse() {
+        return;
+    }
+}
