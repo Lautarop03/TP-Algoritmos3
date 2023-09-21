3504df729f43c80f3e913a139c50b6bfe253f787
Lautarop03 <Lautaropedrozo2@gmail.com>
2023-09-21 20:02:19 -0300

    Agrego modificacion en Pokemon y PokemonView

--- src/main/java/org/fiuba/algoritmos3/pokemon/Pokemon.java
+++ src/main/java/org/fiuba/algoritmos3/pokemon/Pokemon.java
@@ -1,7 +1,8 @@
-package org.fiuba.algoritmos3;
+package org.fiuba.algoritmos3.pokemon;
 
+import org.fiuba.algoritmos3.pokemon.Tipo;
 import org.fiuba.algoritmos3.habilidades.Habilidad;
-import org.fiuba.algoritmos3.estados.Estado;
+import org.fiuba.algoritmos3.pokemon.estados.Estado;
 
 
 import java.util.List;
@@ -19,4 +20,47 @@ public class Pokemon {
     private Estado estado;
     private List<Habilidad> habilidades;
 
+    public String getNombre() {
+        return nombre;
+    }
+
+    public Integer getNivel() {
+        return nivel;
+    }
+
+    public Tipo getTipo() {
+        return tipo;
+    }
+
+    public String getHistoria() {
+        return historia;
+    }
+
+    public Integer getVidaMaxima() {
+        return vidaMaxima;
+    }
+
+    public Integer getVidaActual() {
+        return vidaActual;
+    }
+
+    public Integer getVelocidad() {
+        return velocidad;
+    }
+
+    public Integer getDefensa() {
+        return defensa;
+    }
+
+    public Integer getAtaque() {
+        return ataque;
+    }
+
+    public Estado getEstado() {
+        return estado;
+    }
+
+    public List<Habilidad> getHabilidades() {
+        return habilidades;
+    }
 }
--- src/main/java/org/fiuba/algoritmos3/views/PokemonView.java
+++ src/main/java/org/fiuba/algoritmos3/views/PokemonView.java
@@ -1,5 +1,22 @@
-package org.fiuba.algoritmos3;
+package org.fiuba.algoritmos3.views;
+
+import org.fiuba.algoritmos3.pokemon.Pokemon;
 
 public class PokemonView {
+
+    private Pokemon pokemon;
+
+    public void imprimirPokemon(){
+        System.out.printf("Pokemon: : %s",pokemon.getNombre());
+        System.out.printf("Nivel: %d",pokemon.getNivel());
+        System.out.printf("Tipo: %s",pokemon.getTipo());
+        System.out.printf("Vida Maxima: %d",pokemon.getVidaMaxima());
+        System.out.printf("Vida actual: %d",pokemon.getVidaActual());
+        System.out.printf("Velocidad: %d",pokemon.getVelocidad());
+        System.out.printf("Defensa: %d",pokemon.getDefensa());
+        System.out.printf("Ataque: %d",pokemon.getAtaque());
+        //System.out.printf("Estado: %d",pokemon.getEstado());
+        //System.out.printf("Habilidades: %d",pokemon.getHabilidades());
+    }
 
 }
