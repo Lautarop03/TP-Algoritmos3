e84ca11102f096f63d575459fa9c232107ec946b
Lautarop03 <Lautaropedrozo2@gmail.com>
2023-10-05 21:11:14 -0300

    Elimino interfaz innecesaria

--- src/main/java/org/fiuba/algoritmos3/views/estadoView/EstadoView.java
+++ src/main/java/org/fiuba/algoritmos3/views/estadoView/EstadoView.java
@@ -4,7 +4,7 @@ import org.fiuba.algoritmos3.model.pokemon.Pokemon;
 import org.fiuba.algoritmos3.model.pokemon.estados.Estado;
 
 
-public abstract class EstadoView implements MostrarEstado {
+public abstract class EstadoView {
     public abstract void mostrarEstado(Pokemon pokemon, Estado estado, boolean aplicado);
 
 }
--- src/main/java/org/fiuba/algoritmos3/views/estadoView/MostrarEstado.java
+++
@@ -1,8 +0,0 @@
-package org.fiuba.algoritmos3.views.estadoView;
-
-import org.fiuba.algoritmos3.model.pokemon.estados.*;
-import org.fiuba.algoritmos3.model.pokemon.Pokemon;
-
-public interface MostrarEstado {
-    void mostrarEstado(Pokemon pokemon, Estado estado, boolean aplicado);
-}
