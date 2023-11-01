cacb2fe1ec8200d9d90d6e28b9444eccbf88f5cc
Kevin-Otalvares <kevinotalvares@gmail.com>
2023-10-31 22:21:22 -0300

    Tests unit. de items de ataque y defensa

--- src/test/java/unit.model/items/ItemDeDefensaTest.java
+++ src/test/java/unit.model/items/ItemDeDefensaTest.java
@@ -1,8 +1,58 @@
 package unit.model.items;
 
-import static org.mockito.ArgumentMatchers.anyInt;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
+import org.fiuba.algoritmos3.model.items.ItemModificadorDefensa;
+import org.fiuba.algoritmos3.model.pokemon.Pokemon;
+import org.fiuba.algoritmos3.model.pokemon.Tipo;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.*;
 
 public class ItemDeDefensaTest {
+
+    private ItemModificadorDefensa itemModificadorDefensa = new ItemModificadorDefensa(1);
+    private Pokemon pokemon = spy(new Pokemon("",0, Tipo.Electrico, "", 100,100,0,0.0,0.0,null));
+
+    @Test
+    public void testModificarDefensaPokemonVivo() {
+        //Arrange
+        pokemon.setDefensa(100.0);
+        double nuevaDefensa = 110.0;
+
+        //Act
+        Boolean resultado = itemModificadorDefensa.aplicarItem(pokemon);
+
+        //Assert
+        assertEquals(false, resultado);
+        assertTrue(nuevaDefensa == pokemon.getAtaque());
+    }
+
+    @Test
+    public void testModificarDefensaPokemonMuerto() {
+        //Arrange
+        pokemon.setDefensa(100.0);
+        double nuevaDefensa = 110.0;
+        pokemon.setVidaActual(0);
+
+        //Act
+        Boolean resultado = itemModificadorDefensa.aplicarItem(pokemon);
+
+        //Assert
+        assertEquals(false, resultado);
+        assertTrue(nuevaDefensa == pokemon.getAtaque());
+    }
+
+    @Test
+    public void testItemDeDefensaCantidadDespuesDeUso() {
+        //Arrange
+        pokemon.setDefensa(100.0);
+
+        //Act
+        Boolean resultado = itemModificadorDefensa.aplicarItem(pokemon);
+
+        //Assert
+        assertEquals(false, resultado);
+        assertTrue(itemModificadorDefensa.getCantidad() == 0);
+    }
 }
--- src/test/java/unit.model/items/ItemDeEstadisticaTest.java
+++
@@ -1,8 +0,0 @@
-package unit.model.items;
-
-import static org.mockito.ArgumentMatchers.anyInt;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
-
-public class ItemDeEstadisticaTest {
-}
