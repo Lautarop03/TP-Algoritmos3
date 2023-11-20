1bdb4395f1aed0ee283586ed3a97471fc7333d3b
Lautarop03 <Lautaropedrozo2@gmail.com>
2023-11-20 19:54:53 -0300

    refactor de imagenes

--- src/main/java/org/fiuba/algoritmos3/MainFX.java
+++ src/main/java/org/fiuba/algoritmos3/MainFX.java
@@ -1,8 +1,10 @@
 package org.fiuba.algoritmos3;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -10,21 +12,16 @@ import java.io.IOException;
 public class MainFX extends Application {
     @Override
     public void start(Stage stage) throws IOException{
-//        FXMLLoader = new FXMLLoader(HelloApplication.class.getResource("battleMain.fxml"));
-//        Scene = new Scene(fxmlLoader.load(),450,600);
+//        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("battleMain.fxml"));
+//        Scene scene = new Scene(fxmlLoader.load(),450,600);
 //        stage.setTitle("Hello World!");
 //        stage.setScene(scene);
 //        stage.show();
+
         stage.setTitle("Hello World!");
         StackPane root = new StackPane();
         stage.setScene(new Scene(root, 300, 250));
         stage.show();
 
     }
-
-     public static void main(String[] args){
-        launch();
-     }
-
-
 }
