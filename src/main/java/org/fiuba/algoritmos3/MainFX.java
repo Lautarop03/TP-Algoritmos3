4fa455510ea00c0044b0ddbfa94b57d9417523f0
MateXOre <matiasmendiola18@gmail.com>
2023-11-21 04:45:04 -0300

    mochila fxml falta terminar

--- src/main/java/org/fiuba/algoritmos3/MainFX.java
+++ src/main/java/org/fiuba/algoritmos3/MainFX.java
@@ -4,6 +4,8 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.layout.*;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -11,11 +13,28 @@ import java.io.IOException;
 public class MainFX extends Application {
     @Override
     public void start(Stage stage) throws IOException{
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("itemsFx.fxml"));
-        Parent root = loader.load();
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("battleMain.fxml"));
+        VBox root = loader.load();
 
-        Scene scene = new Scene(root, 640, 700);
+        Scene scene = new Scene(root, 600, 450);
+
         stage.setScene(scene);
+        stage.show();
+
+        HBox pelea = (HBox) scene.lookup("#batalla");
+
+        Image image = new Image(getClass().getResource("background/battlebgroute.jpg").toString());
+        BackgroundImage bgImage = new BackgroundImage(
+                image,
+                BackgroundRepeat.NO_REPEAT,
+                BackgroundRepeat.NO_REPEAT,
+                BackgroundPosition.DEFAULT,
+                new BackgroundSize(
+                        root.getHeight(), root.getWidth(), true, true, true, true)
+        );
+
+        Background bg = new Background(bgImage);
+        pelea.setBackground(bg);
 
         stage.show();
 
--- src/main/java/org/fiuba/algoritmos3/controller/JuegoController.java
+++ src/main/java/org/fiuba/algoritmos3/controller/JuegoController.java
@@ -1,13 +1,20 @@
 package org.fiuba.algoritmos3.controller;
 
+import javafx.fxml.FXML;
 import org.fiuba.algoritmos3.model.Juego;
 
 public class JuegoController {
+
+
+    @FXML
+
     private Juego juego;
 
     //TODO: terminar de definir los atributos
 
     public void setJuego(Juego juego) {
+        this.juego = juego;
+
         // TODO: aca seteo todos los datos de la pantalla
     }
 
