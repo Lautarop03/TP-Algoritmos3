4f1446dd413d951228dac63d264e70f1189df71b
Kevin Otalvares <kotalvares@fi.uba.ar>
2023-11-26 22:15:16 -0300

    Mostrar Items en MochilaController

--- src/main/java/org/fiuba/algoritmos3/controller/MochilaController.java
+++ src/main/java/org/fiuba/algoritmos3/controller/MochilaController.java
@@ -2,6 +2,7 @@ package org.fiuba.algoritmos3.controller;
 
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Pos;
 import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
@@ -35,20 +36,34 @@ public class MochilaController {
     public void setItems(List<Item> items, Stage stage) throws IOException {
         this.items = items;
 
+        for (Item item : items) {
+            mostrarHileraItem(item);
+        }
+
+        Scene scene =  new Scene(contenedorPrincipal);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    private void mostrarHileraItem(Item item){
         HBox hbox = new HBox();
-        Button boton = new Button("Objeto");
+
+        Button boton = new Button(item.getNombre());
         boton.setPrefSize(236, 50);
+
         Pane pane = new Pane();
-        Label label = new Label("Cantidad");
+        pane.setPrefSize(117,50);
+//        pane.setStyle("-fx-background-color: #009900");
+        Label label = new Label(item.getCantidad().toString());
+        label.setAlignment(Pos.CENTER);
+        label.setLayoutY(6);
+        label.setPrefSize(73, 37);
+//        label.setStyle("-fx-background-color: #000099");
 
         pane.getChildren().add(label);
         hbox.getChildren().addAll(boton, pane);
 
         itemsContainer.getChildren().addAll(hbox);
-
-        Scene scene =  new Scene(contenedorPrincipal);
-        stage.setScene(scene);
-        stage.show();
     }
 
 }
