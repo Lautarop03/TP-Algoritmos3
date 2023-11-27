52069d00f03a24ec6aa07dd4524a8bb3ff72d5c1
Kevin Otalvares <kotalvares@fi.uba.ar>
2023-11-27 00:54:30 -0300

    Estilos Items en mochila.fxml

--- src/main/java/org/fiuba/algoritmos3/controller/MochilaController.java
+++ src/main/java/org/fiuba/algoritmos3/controller/MochilaController.java
@@ -2,20 +2,22 @@ package org.fiuba.algoritmos3.controller;
 
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.Pane;
-import javafx.scene.layout.VBox;
+import javafx.scene.image.Image;
+import javafx.scene.layout.*;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 import org.fiuba.algoritmos3.model.items.Hiperpocion;
 import org.fiuba.algoritmos3.model.items.Item;
 import org.fiuba.algoritmos3.model.items.ItemRestauradorDeVida;
 
 import java.io.IOException;
+import java.util.Collections;
 import java.util.List;
 
 public class MochilaController {
@@ -27,6 +29,8 @@ public class MochilaController {
     public VBox itemsContainer;
     @FXML
     public VBox contenedorPrincipal;
+    @FXML
+    public HBox fondoMochila;
 
     private List<Item> items;
 
@@ -35,6 +39,22 @@ public class MochilaController {
 
     public void setItems(List<Item> items, Stage stage) throws IOException {
         this.items = items;
+
+        Image image = new Image(getClass().getResource("/org/fiuba/algoritmos3/background/mochila.png").toString());
+        BackgroundImage bgImage = new BackgroundImage(
+                image,
+                BackgroundRepeat.NO_REPEAT,
+                BackgroundRepeat.NO_REPEAT,
+                BackgroundPosition.DEFAULT,
+                new BackgroundSize(
+                        BackgroundSize.AUTO, BackgroundSize.AUTO, true, true, true, true)
+        );
+        BackgroundFill bgf = new BackgroundFill(
+                Color.WHITE,
+                new CornerRadii(0),
+                new Insets(0));
+        Background bg = new Background(Collections.singletonList(bgf), Collections.singletonList(bgImage));
+        fondoMochila.setBackground(bg);
 
         for (Item item : items) {
             mostrarHileraItem(item);
